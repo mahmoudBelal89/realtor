03e2330e77952fcb12959a4cd6bd66f6814f510b
mahmoud belal <mahmoudbelal89@gmail.com>
2023-01-16 17:44:38 +0200

    before delete

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react';
+
 export default function SignIn() {
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const { email, password } = formData;
+  function onChange(e) {
+    setFormData((previousState) => ({
+      ...previousState,
+      [e.target.id]: e.target.value,
+    }));
+  }
   return (
-    <div>Sign In</div>
-  )
+    <section>
+      <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
+      <div className='flex justify-center flex-wrap px-6 py-12 max-w-6xl mx-auto items-center'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
+          <img
+            className='w-full rounded-2xl'
+            src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=773&q=80'
+            alt='key'
+          />
+        </div>
+        <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
+          <form>
+            <input
+              className='w-full'
+              type='email'
+              id='email'
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 }
