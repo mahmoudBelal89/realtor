8c9cd550935bcffda731207764d0f77eca38706e
mahmoud <mahmoudbelal89@gmail.com>
2023-01-18 19:19:24 +0200

    sign in done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,8 @@ import { doc, updateDoc } from "firebase/firestore";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
+import { FcHome } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const auth = getAuth();
@@ -78,6 +80,18 @@ export default function Profile() {
             </p>
           </div>
         </form>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white uppercase px-7 py-3 font-medium text-sm rounded shadow-md hover:bg-blue-700 hover:shadow-lg active:bg-blue-800 transition duration-150 ease-in-out"
+        >
+          <Link
+            to={"/create-listing"}
+            className="flex justify-center items-center"
+          >
+            <FcHome className="mr-2 text-3xl bg-blue-900 rounded-full p-1 border-2" />
+            Sell or rent your home
+          </Link>
+        </button>
       </div>
     </section>
   );
